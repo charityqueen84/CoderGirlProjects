5854cb895865f9a409183cf93594d476cd9ba98d
charityqueen84 <46950834+charityqueen84@users.noreply.github.com>
2019-02-12 21:57:36 -0600

    Add files via upload

---
+++ VerseAndChorus.java
@@ -0,0 +1,36 @@
+package com.company;
+
+
+public class VerseAndChorus {
+    public static void chorus()
+    {
+        System.out.println("With a knick-knack paddywhack,");
+        System.out.println("Give the dog a bone,");
+        System.out.println("This old man came rolling home.");
+        System.out.println();
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("This old man, he played one.");
+        System.out.println("He played knick-knack on my thumb;");
+        chorus();
+        System.out.println("This old man, he played two.");
+        System.out.println("He played knick-knack on my shoe;");
+        chorus();
+        System.out.println("This old man, he played three.");
+        System.out.println("He played knick-knack on my knee;");
+        chorus();
+        System.out.println("This old man, he played four.");
+        System.out.println("He played knick-knack on my door;");
+        chorus();
+        System.out.println("This old man, he played five.");
+        System.out.println("He played knick-knack on my hive;");
+        chorus();
+        System.out.println("This old man, he played six.");
+        System.out.println("He played knick-knack on my sticks;");
+        chorus();
+
+
+    }
+}
